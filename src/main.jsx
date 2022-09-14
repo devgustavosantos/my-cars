e8699a7c41b1db8bcaa088afbd35e04d6a4040ef
Gustavo Santos <devgustavosantos@gmail.com>
2022-09-14 07:46:45 -0300

    feat: create the wrapper component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { ThemeProvider } from "styled-components";
+
+import GlobalStyle from "./styles/global";
+import Theme from "./styles/theme";
+
+import { Details } from "./pages/Details";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
 );
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+import { Wrapper } from "../../components/Wrapper";
+import { Header } from "../../components/Header";
+
+export function Details() {
+  return (
+    <Container>
+      <Wrapper>
+        <Header />
+      </Wrapper>
+    </Container>
+  );
+}
